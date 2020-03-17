c61c118998806fee80397de0eebf39e8ce60759b
sodiq oyedotun <oyedotunsodiq045@gmail.com>
2020-03-17 01:20:01 +0100

    Solution 2

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # mongo-crud-demo-exercise
 
+Exercise 1
 Get all the published backend courses,
 sort them by their name,
+pick only their name and author,
+and display them
+
+Exercise 2
+Get all the published frontend and backend courses,
+sort them by their price in a descending order,
 pick only their name and author,
 and display them
---
+++ solution2.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose
+  .connect('mongodb://localhost/mongo-exercises')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.log('Could not connect to MongoDB...', err));
+
+const CourseSchema = new Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  price: Number,
+  isPublished: Boolean
+});
+
+// Course Model
+const Course = mongoose.model('Course', CourseSchema);
+
+async function getCourses() {
+  const courses = await Course
+    // Get all the published backend courses,
+    // .find({ isPublished: true, tags: { $in: ['frontend', 'backend'] } })
+    .find({ isPublished: true })
+    .or([{ tags: 'frontend' }, { tags: 'backend' }])
+    // sort them by their price in a descending order,
+    // .sort('-price')
+    .sort({ price: -1 })
+    // pick only their name and author,
+    // .select('name author');
+    .select({ name: 1, author: 1 });
+
+  // and display them
+  console.log(courses);
+}
+
+getCourses();
